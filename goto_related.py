72280d89c354fb25b76ec765740720fb0739d67a
Mike Schreifels <mike@schreifels.org>
2016-03-25 18:49:42 -0700

    Add goto_related_patterns_to_strip option

--- goto_related.py
+++ goto_related.py
@@ -3,19 +3,38 @@ try:
 except ImportError:
     from test.stubs import sublime, sublime_plugin
 
-import os
+import os, re
 
 class GotoRelatedCommand(sublime_plugin.TextCommand):
 
+    DEBUG = False
+
     def run(self, edit):
-        filename = self.clean_path(self.current_file_path())
+        current_path = self.current_file_path()
+
+        if self.DEBUG:
+            print('GotoRelated: transforming %s' % current_path)
+
+        filename = self.clean_path(current_path)
         self.show_goto(filename)
 
     def clean_path(self, path):
-        return os.path.basename(path)
+        filename = os.path.basename(path)
+
+        for pattern in self.patterns_to_strip():
+            if self.DEBUG:
+                print('  - Replacing /%s/ in "%s"' % (pattern, filename))
+            filename = re.sub(pattern, '', filename)
+
+        return filename
 
     def current_file_path(self):
-        return self.view.file_name()
+        return self.view.file_name() or ''
+
+    def patterns_to_strip(self):
+        return self.view.window().project_data().get('goto_related_patterns_to_strip') or \
+                    self.view.settings().get('goto_related_patterns_to_strip') or \
+                    []
 
     def show_goto(self, initial_text):
         self.view.window().run_command('show_overlay', { 'overlay': 'goto', 'text': initial_text })
--- test/test_goto_related.py
+++ test/test_goto_related.py
@@ -8,17 +8,26 @@ sys.path.insert(0, root_path)
 from test.stubs import sublime, sublime_plugin
 from goto_related import GotoRelatedCommand
 
-class TestGotoRelated(unittest.TestCase):
+class TestGotoRelatedCommand(unittest.TestCase):
 
-    def setUp(self):
-        self.subject = GotoRelatedCommand()
-        self.subject.current_file_path = Mock(return_value='/Users/mike/projects/example/app/main.py')
-        self.subject.show_goto = Mock()
-        self.edit = Mock()
+    def assert_goto(self, current_file_path, patterns_to_strip, expected_overlay_text):
+        subject = GotoRelatedCommand()
+
+        subject.show_goto = Mock()
+        subject.current_file_path = Mock(return_value=current_file_path)
+        subject.patterns_to_strip = Mock(return_value=patterns_to_strip)
+
+        subject.run(Mock())
+        subject.show_goto.assert_called_with(expected_overlay_text)
 
     def test_run(self):
-        self.subject.run(self.edit)
-        self.subject.show_goto.assert_called_with('main.py')
+        self.assert_goto('/path/to/main.rb',              [],                  'main.rb')
+        self.assert_goto('/path/to/main_test.rb',         [],                  'main_test.rb')
+        self.assert_goto('/path/to/my_test_test.rb',      ['_test'],           'my.rb')
+        self.assert_goto('/path/to/my_test_test.rb',      ['_test$'],          'my_test_test.rb')
+        self.assert_goto('/path/to/my_test_test.rb',      ['\..+$', '_test$'], 'my_test')
+        self.assert_goto('/path/to/home.html.erb',        ['\..+$'],           'home')
+        self.assert_goto('/path/to/_my_partial.html.erb', ['^_', '\..+$'],     'my_partial')
 
 if __name__ == '__main__':
     unittest.main()
